7af9eebeaf99f2c0aa62db402c534b2874624f14
Abhinav Khare <abhinav@Abhinavs-MacBook-Pro.local>
2016-12-23 17:42:54 +0530

    Major structural changes

--- js/shuffle.js
+++ js/shuffle.js
@@ -1,19 +1,41 @@
+
 setTimeout(function(){
 var $ = function(id) {
   return document.getElementById(id);
 };
 var inc = 0;
 var out = 0;
-var str = 'Coming This March.......';
+var str = 'Coming This Spring.......@23^ - 25* march\'17';
 var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@$%&';
 var t;
-$('Terminal').style.display = 'none';
+$('Terminal').style.opacity=0;
 var anim = function() {
   inc++;
   if (inc % 7 === 0 && out < str.length) {
+    if(str[out]=='@')
+    {
+     // var lbreak  = document.createElement("BR");
+      
+     // $('anim').appendChild(lbreak);
+
+    }
+    else if(str[out]=='^')
+    {
+      var sup1  = document.createElement("SUP");
+      sup1.appendChild(document.createTextNode("rd"));
+      $('anim').appendChild(sup1);
+    }
+    else if(str[out]=='*')
+    {
+      var sup2  = document.createElement("SUP");
+      sup2.appendChild(document.createTextNode("th"));
+      $('anim').appendChild(sup2);
+    }
+    else
     $('anim').appendChild(document.createTextNode(str[out]));
     out++;
-  } else if (out >= str.length) {
+  }
+   else if (out >= str.length) {
     clearInterval(t);    
     $('shuffle').innerHTML = '';  
   }
@@ -33,4 +55,3 @@ $('shuffle').innerHTML = '';
 }
 document.getElementById('btn').onclick = reload;*/
 },13000);
-
