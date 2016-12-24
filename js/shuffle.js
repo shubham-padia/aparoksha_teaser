d46f9d16a24ebb6b1d118e99df123a268370501e
shubham-padia <shubhamapadia@gmail.com>
2016-12-25 01:18:01 +0530

    Make animation faster and change shuffle text

--- js/shuffle.js
+++ js/shuffle.js
@@ -1,41 +1,19 @@
-
 setTimeout(function(){
 var $ = function(id) {
   return document.getElementById(id);
 };
 var inc = 0;
 var out = 0;
-var str = 'Coming This Spring.......@23^ - 25* march\'17';
+var str = '23-25 March - 2017';
 var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@$%&';
 var t;
-$('Terminal').style.opacity=0;
+$('Terminal').style.display = 'none';
 var anim = function() {
   inc++;
   if (inc % 7 === 0 && out < str.length) {
-    if(str[out]=='@')
-    {
-     // var lbreak  = document.createElement("BR");
-      
-     // $('anim').appendChild(lbreak);
-
-    }
-    else if(str[out]=='^')
-    {
-      var sup1  = document.createElement("SUP");
-      sup1.appendChild(document.createTextNode("rd"));
-      $('anim').appendChild(sup1);
-    }
-    else if(str[out]=='*')
-    {
-      var sup2  = document.createElement("SUP");
-      sup2.appendChild(document.createTextNode("th"));
-      $('anim').appendChild(sup2);
-    }
-    else
     $('anim').appendChild(document.createTextNode(str[out]));
     out++;
-  }
-   else if (out >= str.length) {
+  } else if (out >= str.length) {
     clearInterval(t);    
     $('shuffle').innerHTML = '';  
   }
@@ -46,7 +24,7 @@ var anim = function() {
     $('shuffle').innerHTML = '';  
   }
 };
-t = setInterval(anim, 50);
+t = setInterval(anim, 25);
 $('shuffle').innerHTML = '';
 
 //Do it again
